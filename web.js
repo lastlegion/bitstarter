5eeb6cfca1e11fb9e1e90be2a49e25f32cdbf0bf
Ganesh Iyer <lastlegion@gmail.com>
2013-07-20 23:20:20 +0530

    read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+	var page_data = fs.readFileSync('index.html');	
+	console.log(page_data, 'utf-8');
+	page_data = page_data.toString();
+	response.send(page_data);
 });
 
 var port = process.env.PORT || 5000;
