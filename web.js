48df9acd825ad62b8e8a06a902d8ea0922be4e95
lastlegion <lastlegion@gmail.com>
2013-07-21 23:00:30 +0530

    wireframe created

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var app = express();
+var path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
